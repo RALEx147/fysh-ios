dcd1376b0802173f62b1ee067a45373b1d2120c6
jag070 <jag070@bucknell.edu>
2020-09-20 17:16:46 -0400

    can push to dynamo

--- Pods/Target Support Files/Pods-fysh-ios/Pods-fysh-ios-frameworks.sh
+++ Pods/Target Support Files/Pods-fysh-ios/Pods-fysh-ios-frameworks.sh
@@ -197,6 +197,7 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSAppSync/AWSAppSync.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSAuthCore/AWSAuthCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProvider/AWSCognitoIdentityProvider.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProviderASF/AWSCognitoIdentityProviderASF.framework"
@@ -216,6 +217,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSAppSync/AWSAppSync.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSAuthCore/AWSAuthCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProvider/AWSCognitoIdentityProvider.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProviderASF/AWSCognitoIdentityProviderASF.framework"
--- fysh-ios/ContentView.swift
+++ fysh-ios/ContentView.swift
@@ -7,14 +7,23 @@
 //
 
 import SwiftUI
+import Amplify
+import AmplifyPlugins
+import AWSAppSync
 
 struct ContentView: View {
     @State private var selection = 0
- 
+    
+
     var body: some View {
+        
+        
         TabView(selection: $selection){
             Text("First View")
                 .font(.title)
+                .onAppear{
+                    self.performOnAppear()
+                }
                 .tabItem {
                     VStack {
                         Image("first")
@@ -33,10 +42,46 @@ struct ContentView: View {
                 .tag(1)
         }
     }
+    
+    func performOnAppear() {
+        let item = Record( id: "877", temp: "60", latitude: "50.2", longitude: "172", time: "5:00 AM")
+        
+        Amplify.DataStore.save(item) { (result) in
+           switch(result) {
+           case .success(let savedItem):
+               print("Saved item: \(savedItem.temp)")
+           case .failure(let error):
+               print("Could not save item to datastore: \(error)")
+           }
+        }
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        var appSyncClient: AWSAppSyncClient?
+        
+        appSyncClient = appDelegate.appSyncClient
+        
+        let mutationInput = CreateRecordInput( temp: "66", latitude: "56.4", longitude: "123", time: "8:00")
+
+        appSyncClient?.perform(mutation: CreateRecordMutation(input: mutationInput)) { (result, error) in
+          if let error = error as? AWSAppSyncClientError {
+            print("Error occurred: \(error.localizedDescription )")
+          }
+          if let resultError = result?.errors {
+            print("Error saving the item on server: \(resultError)")
+            return
+          }
+        }
+        
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
+
 }
