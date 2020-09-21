85258f65d2ea8e6573ed484ec09569d3a82e1c13
Robert Alexander <rla@navadrag.com>
2020-09-20 21:07:55 -0400

    add placeholder UI elements for 3 buttons

--- fysh-ios/ViewController.swift
+++ fysh-ios/ViewController.swift
@@ -14,15 +14,23 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 40.957531, longitude: -76.886787), zoomLevel: 13, animated: false)
-		mapView.logoView.isHidden = true
-		mapView.attributionButton.isHidden = true
-        view.addSubview(mapView)
+		addUIMapbox()
+		addUIMenu()
+		addUISearch()
+		addUIInput()
 	}
 
-
+	@objc func pressedMenu(){
+		print("Menu button tapped")
+	}
+	
+	@objc func pressedSearch(){
+		print("Search button tapped")
+	}
+	
+	@objc func pressedInput(){
+		print("Input button tapped")
+	}
+	
 }
 
